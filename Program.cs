406660d65dc242ab23296f7b39afcc0e152d2ba8
danishahmadcode <danishaltafpro@gmail.com>
2024-03-23 11:23:00 +0530

    Some Progress

--- Program.cs
+++ Program.cs
@@ -1,8 +1,36 @@
-﻿namespace test;
-class Program
+﻿namespace Arithmetic{
+public class Operations
 {
+    public int PlusOperator(int num1, int num2){
+        return num1 + num2;
+    }
+    public int MinusOperator(int num1, int num2){
+        return num1 - num2;
+    }
+    public int DivideOperator(int num1, int num2){
+        return num1 / num2;
+    }
+    public int MultiplyOperator(int num1, int num2){
+        return num1 * num2;
+    }
+    public int ModulusOperator(int num1, int num2){
+        return num1 % num2;
+    }    
+}
+}
+
+namespace MainNamespace{
+    using Arithmetic;
+    class MyMainClass{
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Danish");
+        Operations value = new Operations();
+        Console.WriteLine(value.PlusOperator(10,20));
+        Console.WriteLine(value.MinusOperator(10,20));
+        Console.WriteLine(value.DivideOperator(10,20));
+        Console.WriteLine(value.MultiplyOperator(10,20));
+        Console.WriteLine(value.ModulusOperator(10,20));
+    }
     }
 }
+
